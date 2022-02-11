c14a4dd93de4c071c0e369c9595810a57e2d0e40
mattia pietropaolo <mpp.pietropaolo@gmail.com>
2022-02-11 14:46:48 +0000

    created style for components and main template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  private readonly recipe1: Recipe = new Recipe(
+    'Vegan Chocolate Cake',
+    'The indulgent, fudgy vegan chocolate cake',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chocolate-avocado-cake-51c276b.jpg?quality=90&webp=true&resize=440,400'
+  );
 
-  constructor() { }
+  private readonly recipe2: Recipe = new Recipe(
+    'Vegan Chocolate Cake',
+    'The indulgent, fudgy vegan chocolate cake',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chocolate-avocado-cake-51c276b.jpg?quality=90&webp=true&resize=440,400'
+  );
 
-  ngOnInit(): void {
-  }
+  recipes: Recipe[] = [this.recipe1, this.recipe2];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
